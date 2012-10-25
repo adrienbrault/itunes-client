eabd93a713970f50f9e1f690562b4dbef4b28170
Adrien Brault <adrien.brault@gmail.com>
2012-10-25 02:16:53 +0200

    Add AvailableFeedsCommand

---
+++ src/AdrienBrault/ItunesClient/Command/AvailableFeedsCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace AdrienBrault\ItunesClient\Command;
+
+use Guzzle\Service\Command\OperationCommand;
+use Guzzle\Service\Command\CommandInterface;
+use Guzzle\Service\Description\Operation;
+use Guzzle\Service\Command\ResponseParserInterface;
+
+/**
+ * @author Adrien Brault <adrien.brault@gmail.com>
+ *
+ * Look at the source code of @link http://itunes.apple.com/rss
+ */
+class AvailableFeedsCommand extends OperationCommand implements ResponseParserInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function init()
+    {
+        $this->setResponseParser($this);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function createOperation()
+    {
+        return new Operation(array(
+            'name'       => 'available_feeds_command',
+            'uri'        => '/WebObjects/MZStoreServices.woa/wa/RSS/wsAvailableFeeds?cc={country}',
+            'httpMethod' => 'GET',
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function parse(CommandInterface $command)
+    {
+        $content = $command->getRequest()->getResponse()->getBody(true);
+
+        return json_decode(preg_replace('/^availableFeeds=/', '', $content), true);
+    }
+}
---
+++ tests/AdrienBrault/ItunesClient/Tests/Command/AvailableFeedsCommandTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AdrienBrault\ItunesClient\Tests\Command;
+
+use AdrienBrault\ItunesClient\Tests\TestCase;
+
+class AvailableFeedsCommandTest extends TestCase
+{
+    public function testUri()
+    {
+        $client = $this->getClient();
+
+        $command = $client->getCommand('available_feeds_command');
+
+        $request = $command->prepare();
+
+        $this->assertEquals('/WebObjects/MZStoreServices.woa/wa/RSS/wsAvailableFeeds?cc=us', $request->getResource());
+    }
+
+    public function testExecute()
+    {
+        $client = $this->getClient();
+        $this->setMockResponse($client, 'available_feeds_us');
+
+        $command = $client->getCommand('available_feeds_command');
+
+        $result = $command->execute();
+
+        $this->assertInternalType('array', $result);
+        $this->assertArrayHasKey('list', $result);
+    }
+}
